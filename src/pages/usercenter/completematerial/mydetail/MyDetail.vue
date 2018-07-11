bca37a2f0379ddc05632c89c5e834b72734077d6
mengxi wu <412456374@qq.com>
2018-07-11 17:04:30 +0800

    增加“交友记录”、“修改密码”页面

--- src/pages/usercenter/completematerial/mydetail/MyDetail.vue
+++ src/pages/usercenter/completematerial/mydetail/MyDetail.vue
@@ -1,13 +1,31 @@
 <template>
-  <div>详细资料</div>
+  <div class="my-detail-root">
+    <user-header>
+      <span slot="title">详细资料</span>
+    </user-header>
+    <div class="detail-content-container"></div>
+  </div>
 </template>
 
 <script>
-export default {
+import UserHeader from '@/components/UserHeader'
 
+export default {
+  components: { UserHeader }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+.my-detail-root {
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  .detail-content-container {
+    width: 100%;
+    height: 200px;
+    border: 1px solid black;
+    box-sizing: border-box;
+  }
+}
 </style>
--- src/pages/usercenter/myaccount/modules/EditPassword.vue
+++ src/pages/usercenter/myaccount/modules/EditPassword.vue
@@ -1,13 +1,137 @@
 <template>
-  <div>修改密码</div>
+  <div class="edit-password-root">
+    <user-header>
+      <span slot="title">密码修改</span>
+    </user-header>
+    <div class="edit-content-container">
+      <div class="account-edit-content">
+        <div class="item-row">
+          <div class="row-label">手机号:</div>
+          <div class="row-content">
+            <span>152****7571</span>
+            <otp-btn class="otp-btn" :phoneNumber="phone"></otp-btn>
+          </div>
+        </div>
+        <div class="item-row">
+          <div class="row-label">手机验证码:</div>
+          <div class="row-content">
+            <el-input placeholder="请输入验证码" v-model="vecode" maxlength="4"></el-input>
+          </div>
+        </div>
+        <div class="item-row">
+          <div class="row-label"></div>
+          <div class="row-content">
+            <div class="vecode-remark">
+              验证码已发送。超过60秒未收到短信校验码，可点击重新发送按钮来重新获取短信校验码。
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="account-edit-content" style="border: none">
+        <div class="item-row">
+          <div class="row-label" style="font-size: 12px">请重新设置登录密码</div>
+        </div>
+        <div class="item-row">
+          <div class="row-label">设置新密码:</div>
+          <div class="row-content">
+            <el-input type="password" placeholder="6-20位字母、数字组合" v-model="newPwd"
+              maxlength="20" auto-complete="new-password"></el-input>
+          </div>
+        </div>
+        <div class="item-row">
+          <div class="row-label">确认新密码:</div>
+          <div class="row-content">
+            <el-input type="password" placeholder="再次填写密码" v-model="againPwd"
+              maxlength="20" auto-complete="new-password"></el-input>
+          </div>
+        </div>
+      </div>
+      <div class="btn-box">确 定</div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import UserHeader from '@/components/UserHeader'
+import OtpBtn from '@/components/OtpBtn'
 
+export default {
+  components: { UserHeader, OtpBtn },
+  data () {
+    return {
+      phone: '',
+      vecode: '',
+      stepNum: '1',
+      newPwd: '',
+      againPwd: ''
+    }
+  }
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+.edit-password-root {
+  width: 100%;
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  .edit-content-container {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 10px;
+    display: flex;
+    flex-direction: column;
+    .account-edit-content {
+      margin: 15px 0;
+      width: 100%;
+      box-sizing: border-box;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      justify-content: center;
+      padding: 10px 20px 0 10px;
+      border-bottom: 1px solid #F3F3F3;
+      .item-row {
+        width: 100%;
+        box-sizing: border-box;
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        margin-bottom: 20px;
+        .row-label {
+          font-size: 14px;
+          width: 120px;
+          text-align: left;
+        }
+        .row-content {
+          width: 300px;
+          height: 100%;
+          box-sizing: border-box;
+          display: flex;
+          align-items: center;
+          .otp-btn{
+            margin-left: 15px;
+          }
+          .vecode-remark {
+            width: 100%;
+            text-align: left;
+            color: grey;
+            font-size: 14px;
+          }
+        }
+      }
+    }
+    .btn-box {
+      margin: 20px 0 100px 0;
+      width: 100%;
+      height: 40px;
+      background-color: #F1356F;
+      box-sizing: border-box;
+      text-align: center;
+      line-height: 40px;
+      color: white;
+    }
+  }
+}
 
 </style>
